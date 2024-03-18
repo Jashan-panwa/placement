22f943873b5d8f49c41784e0febaefd42c6a7599
Jashan-panwa <jashanpanwa2003@gmail.com>
2024-03-18 11:56:26 +0530

    updated the code

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-from flask import Flask,jsonify,request
-
-app = Flask(__name__)
-
-@app.route('/bfhl', methods=['POST'])
-def welcome():
-    print(request.json["data"])
-    numbers = []
-    alphabets = []
-    for data in request.json["data"]:
-        if data.isalpha():
-            alphabets.append(data)
-        else:
-            numbers.append(data)
-    return jsonify({
-        "is_success": True,
-        "user_id": "john_do_17091999",
-        "email" : "john@xyz.com",
-        "roll_number":"ABCD123",
-        "numbers": numbers,
-        "alphabets": alphabets}
-    )
-
-if __name__ == '__main__':
-    app.run()
+//-------------------------------
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("api is running but your url is not /bfhl");
+});
+app.get("/bfhl", (req, res) => {
+  res.send("WELCOME TO BFHL \n JASHAN_2110990669");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server started on port ${PORT}`));
