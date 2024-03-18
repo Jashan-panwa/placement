f71cf12bb4de0d04a2efefd4e4a6ca831fc91159
Jashan-panwa <jashanpanwa2003@gmail.com>
2024-03-18 12:31:17 +0530

    rollnumber working

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ app.get("/", (req, res) => {
   res.send("api is running but your url is not /bfhl");
 });
 app.get("/bfhl", (req, res) => {
-  res.send("WELCOME TO BFHL \n JASHAN_2110990669");
+  const data = {
+    is_success: true,
+    user_id: "jashan",
+    email: "jashan0669.be21@chitkara.edu.in",
+    roll_number: "2110990669",
+  };
+  res.json(data);
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started on port ${PORT}`));
